f07cf2e5b07415bba2e42b23e8fbd747735f52aa
JINWOO <chaindevchu@gmail.com>
2023-05-06 14:28:40 +0900

    .env 추가

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,26 @@
-import express from 'express';
-const app = express();
-const port = 3000;
+import express, { Application, Request, Response } from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 
-app.get('/', (req: express.Request, res: express.Response) => {
+class Bundle {
+  public app: Application;
+  public host: string;
+  public port: string;
+
+  constructor() {
+    this.app = express();
+    this.host = process.env.HOST || 'http://localhost';
+    this.port = process.env.PORT || '3000';
+  }
+}
+
+const bundle = new Bundle();
+const app = bundle.app;
+
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello World');
 });
 
-app.listen(port, () => {
-  console.log(`app listening on port: ${port}`);
+app.listen(bundle.port, () => {
+  console.log(`THIS APP IS RUNNING ==> ${bundle.host}:${bundle.port}`);
 });
