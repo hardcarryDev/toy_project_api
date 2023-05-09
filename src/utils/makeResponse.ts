634463ecba50b6d428fee8054895089941c8d69c
daramKim <cjw0672@gmail.com>
2023-05-09 18:29:18 +0900

    api 공통 리스폰스 수정

--- src/utils/makeResponse.ts
+++ src/utils/makeResponse.ts
@@ -1,9 +1,9 @@
-function successResponse(data: any) {
+function successResponse(data: { [key: string]: string | number }) {
   return {
     message: 'success',
     result: true,
     code: 200,
-    data: data,
+    data: data ?? {},
   };
 }
 
