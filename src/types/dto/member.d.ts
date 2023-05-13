6e0a8e26df29cb43f9079594d44b8dc3fc04c247
JINWOO <chaindevchu@gmail.com>
2023-05-14 01:22:16 +0900

    필수값 dto 타입 추가 설정

---
+++ src/types/dto/member.d.ts
@@ -0,0 +1,15 @@
+interface memberCreateMustInput {
+  id?: string;
+  user_id?: string;
+  password?: string;
+  name: string | null;
+  gender?: member_gender | null;
+  age?: number | null;
+  email?: string | null;
+  phone?: string | null;
+  grade?: string | null;
+  grade_id?: string | null;
+  create_dt?: Date;
+  update_dt?: Date | null;
+  exit_dt?: Date | null;
+}
