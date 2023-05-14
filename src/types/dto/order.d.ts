e882c81b30ed0f52cc6f9ff54022faffaf6b1c66
JINWOO <chaindevchu@gmail.com>
2023-05-15 00:32:55 +0900

    받는놈 주소, 받는놈 폰번 필수값 처리

--- src/types/dto/order.d.ts
+++ src/types/dto/order.d.ts
@@ -2,8 +2,8 @@ interface orderCreateMustInput {
   id?: string;
   member_id: string;
   req_name?: string | null;
-  res_address?: string | null;
-  res_phone?: string | null;
+  res_address: string | null;
+  res_phone: string | null;
   res_requirement?: string | null;
   total_price?: number | null;
   order_dt?: Date | string | null;
