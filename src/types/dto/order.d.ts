c032fbdad1b8364a1e07917dd1c8512b5f034a5c
JINWOO <chaindevchu@gmail.com>
2023-05-15 00:19:09 +0900

    order must create input 타입 작성

--- src/types/dto/order.d.ts
+++ src/types/dto/order.d.ts
@@ -8,5 +8,25 @@ interface orderCreateMustInput {
   total_price?: number | null;
   order_dt?: Date | string | null;
   order_cancel_dt?: Date | string | null;
-  order_detail?: order_detailCreateNestedManyWithoutOrderInput;
+
+  // 1. order_detail? : Prisma.order_detailCreateWithoutOrderInput
+  // ===> 참조 안됌, any로 인식함. import 문제인줄 알았는데 아님 그냥 안됌.
+
+  // 2. 직접 작성
+  // ==> schema에 __type으로 나옴 그냥 익명 타입으로 만들어주긴 한데 이름이 안보임.
+  // order_detail?: {
+  //   id?: string | undefined;
+  //   product_id: string;
+  //   order_prod_cnt?: number;
+  // };
+
+  // 3. 타입을 아래에 작성
+  // ==> 타입 이름이 표시되고, refs로 연결 가능함.
+  order_detail?: order_detailCreateWithoutOrderInput[];
 }
+
+type order_detailCreateWithoutOrderInput = {
+  id?: string;
+  product_id: string;
+  order_prod_cnt?: number;
+};
