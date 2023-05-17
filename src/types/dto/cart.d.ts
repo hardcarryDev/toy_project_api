b6e26865de8b7d436e35a74bb00b756e8cb6c8d8
JINWOO <chaindevchu@gmail.com>
2023-05-18 05:51:43 +0900

    장바구니 모델 정의

--- src/types/dto/cart.d.ts
+++ src/types/dto/cart.d.ts
@@ -1,4 +1,36 @@
-type cartCreateMustInput = {};
-type cartWhereClientInput = {};
-type cartUpdateMustInput = {};
-type cartDeleteInput = {};
+type cartCreateMustInput = {
+  user_id?: string | null;
+  product_id: string;
+  product_amount?: number | null;
+  product_price?: number | null;
+  status?: string | null;
+};
+
+type cartWhereClientInput = {
+  AND?: Enumerable<cartWhereInput>;
+  OR?: Enumerable<cartWhereInput>;
+  NOT?: Enumerable<cartWhereInput>;
+  id?: string;
+  user_id?: string | null;
+  product_id?: string;
+  product_amount?: number | null;
+  product_price?: number | null;
+  status?: string | null;
+  create_dt?: Date | string | null;
+  update_dt?: Date | string | null;
+  delete_dt?: Date | string | null;
+};
+
+type cartUpdateMustInput = {
+  id: string;
+  user_id?: string | null;
+  product_id?: string;
+  product_amount?: number | null;
+  product_price?: number | null;
+  status?: string | null;
+  create_dt?: Date | string | null;
+  update_dt?: Date | string | null;
+  delete_dt?: Date | string | null;
+};
+
+type cartDeleteInput = ID;
